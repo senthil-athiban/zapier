98941b493e72d9f007cbedd1effae48d75f1d962
Senthil Athiban <senthilathiban0@gmail.com>
2024-07-22 23:37:37 +0530

    (feat): add webhook logic

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,9 +1,24 @@
 import express from "express";
-
+import { PrismaClient } from "@prisma/client";
 const app = express();
-
-app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+const client = new PrismaClient();
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
     const { userId, zapId } = req.params;
+    const metadata = req.body;
+    await client.$transaction(async tx => {
+        const zap = await client.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: metadata
+            }
+        });
+
+        await client.zapRunOutBox.create({
+            data: {
+                zapRunId: zap.id
+            }
+        })
+    });
 })
 
 app.listen(3000);
